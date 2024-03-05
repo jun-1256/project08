226325a735c8a76240929476f5f17187fed28f21
jun-1256 <juns_1201@icloud.com>
2024-03-05 20:42:52 +0900

    2024/03/05

---
+++ TestProject08/src/array3.java
@@ -0,0 +1,17 @@
+
+public class array3 {
+
+	public static void main(String[] args) {
+		// 配列の基礎
+		
+		int[] i = {1,5,10};
+		
+		System.out.println(i[0] + "\n");
+		
+		int[] j = {1,5,10};
+		
+		System.out.println(j[1]);
+
+	}
+
+}
