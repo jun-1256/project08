54b5cb2e6a8138b3dbd99abbf9074337b1dee7b0
jun-1256 <juns_1201@icloud.com>
2024-03-08 22:16:43 +0900

    2024/03/05

---
+++ testproject08/src/Array4.java
@@ -0,0 +1,25 @@
+
+public class Array4 {
+
+	public static void main(String[] args) {
+		// 配列の基礎
+		// データの更新
+		// 一度、配列に格納した、データを更新することも可能。
+		
+		String[] name = {"田中","高橋","斉藤"};
+		
+		name[0]="加藤";
+		
+		System.out.println(name[0] + "\n");
+		
+		String[] rapper = {"Lil Tjay","Blxst","kingvon"};
+		
+		 rapper[1]="Tohji";
+		 
+		 System.out.println(rapper[1]);
+		
+		
+
+	}
+
+}
